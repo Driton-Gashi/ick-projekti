1272141c308d309381680c6ca24a2cccd94b28da
Driton Gashi <dritongashi1995@gmail.com>
2025-11-07 12:57:52 +0100

    Added navigation sidebar on mobile

--- components/header/index.js
+++ components/header/index.js
@@ -4,27 +4,55 @@ class Header extends HTMLElement {
   }
 
   render() {
+    window.openSidebar = () => {
+      const overlay = this.querySelector('.OverlayNavSidebar');
+      overlay.classList.add('open');
+    };
+    window.closeSidebar = () => {
+      const overlay = this.querySelector('.OverlayNavSidebar');
+      overlay.classList.remove('open');
+    };
+
     this.innerHTML = `
-            <header>
-                <div class="logo">
-                    <img src="./assets/images/logo.svg" />
-                </div>
-                <nav>
-                    <li><a class="currentPage" href="#">Home</a></li>
-                    <li><a href="#">Pricing</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">Series</a></li>
-                    <li><a href="#">Collection</a></li>
-                    <li><a href="#">FAQ</a></li>
-                </nav>
-                <div class="iconWrapper">
-                    <my-icon class="burger" iconName="burger"></my-icon>
-                    <my-icon class="search" iconName="search"></my-icon>
-                    <my-icon iconName="bell"></my-icon>
-                    <my-icon iconName="profile"></my-icon>
-                    <my-icon iconName="sun"></my-icon>
-                </div>
-            </header>
+             <header>
+      <div class="logo">
+        <img src="./assets/images/logo.svg" />
+      </div>
+      <nav>
+        <li><a class="currentPage" href="#">Home</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">Series</a></li>
+        <li><a href="#">Collection</a></li>
+        <li><a href="#">FAQ</a></li>
+      </nav>
+      <div onClick="openSidebar()"  class="iconWrapper">
+        <my-icon class="burger" iconName="burger"></my-icon>
+        <my-icon class="search" iconName="search"></my-icon>
+        <my-icon iconName="bell"></my-icon>
+        <my-icon iconName="profile"></my-icon>
+        <my-icon iconName="sun"></my-icon>
+      </div>
+    </header>
+    <div class="OverlayNavSidebar">
+      <div class="NavSidebar">
+        <my-icon onClick="closeSidebar()" class="closeNavSidebarBtn" iconName="close"></my-icon>
+        <img src="./assets/images/logo.svg" />
+        <nav>
+          <li><a class="currentPage" href="#">Home</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Series</a></li>
+          <li><a href="#">Collection</a></li>
+          <li><a href="#">FAQ</a></li>
+        </nav>
+        <div class="NavSidebarIcons">
+          <my-icon iconName="bell"></my-icon>
+          <my-icon iconName="profile"></my-icon>
+          <my-icon iconName="sun"></my-icon>
+        </div>
+      </div>
+    </div>
         `;
   }
 }
