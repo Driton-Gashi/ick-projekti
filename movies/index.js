fd1324abc2d9c44906a0726ffa2ded4db2520dc2
Driton Gashi <dritongashi1995@gmail.com>
2025-11-16 16:31:22 +0100

    series and movies, come from fake DB

--- movies/index.js
+++ movies/index.js
@@ -1,27 +1,30 @@
-const moviesContainer = document.querySelector('.movieList');
+const fetchMovies = async () => {
+  try {
+    const response = await fetch('/data/movies.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Something happened while trying to fetch movies:', error);
+    return [];
+  }
+};
 
-const movies = [
-  { image1: 'movie_1.png', image2: 'movie_2.png' },
-  { image1: 'movie_2.png', image2: 'movie_3.png' },
-  { image1: 'movie_3.png', image2: 'movie_4.png' },
-  { image1: 'movie_4.png', image2: 'movie_5.png' },
-  { image1: 'movie_5.png', image2: 'movie_6.png' },
-  { image1: 'movie_6.png', image2: 'movie_7.png' },
-  { image1: 'movie_7.png', image2: 'movie_8.png' },
-  { image1: 'movie_8.png', image2: 'movie_9.png' },
-  { image1: 'movie_9.png', image2: 'movie_10.png' },
-  { image1: 'movie_10.png', image2: 'movie_11.png' },
-  { image1: 'movie_11.png', image2: 'movie_12.png' },
-  { image1: 'movie_12.png', image2: 'movie_1.png' },
-];
+const loadMovies = async () => {
+  const moviesContainer = document.querySelector('.movieList');
 
-movies.forEach(movie => {
-  moviesContainer.innerHTML += `<my-movie-card
-  onmouseover="movieHoverEffect(event)"
-  onmouseleave="movieMouseLeaveEffect(event)"
-  isMovie
-  image1="${movie.image1}"
-  image2="${movie.image2}"
-  ></my-movie-card>
-`;
-});
+  const movies = await fetchMovies();
+
+  movies.forEach(movie => {
+    moviesContainer.innerHTML += `
+      <my-movie-card
+        onmouseover="movieHoverEffect(event)"
+        onmouseleave="movieMouseLeaveEffect(event)"
+        isMovie
+        image1="${movie.image1}"
+        image2="${movie.image2}"
+      ></my-movie-card>
+  `;
+  });
+};
+
+loadMovies();
--- series/index.js
+++ series/index.js
@@ -1,70 +1,21 @@
-const seriesContainer = document.querySelector('.movieList');
+const fetchSeries = async () => {
+  try {
+    const response = await fetch('/data/series.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Something happened while trying to fetch movies:', error);
+    return [];
+  }
+};
 
-const series = [
-  {
-    image1: 'series_1.png',
-    image2: 'series_2.png',
-    name: 'Eternal Code',
-    rating: 4,
-  },
-  {
-    image1: 'series_2.png',
-    image2: 'series_3.png',
-    name: 'Hidden Truths',
-    rating: 2,
-  },
-  {
-    image1: 'series_3.png',
-    image2: 'series_4.png',
-    name: 'Neon District',
-    rating: 5,
-  },
-  {
-    image1: 'series_4.png',
-    image2: 'series_5.png',
-    name: 'Lost Signal',
-    rating: 3,
-  },
-  {
-    image1: 'series_5.png',
-    image2: 'series_6.png',
-    name: 'Nightfall Legacy',
-    rating: 1,
-  },
-  {
-    image1: 'series_6.png',
-    image2: 'series_7.png',
-    name: 'Codebreaker',
-    rating: 4,
-  },
-  {
-    image1: 'series_7.png',
-    image2: 'series_8.png',
-    name: 'La casa de papel',
-    rating: 5,
-  },
-  {
-    image1: 'series_8.png',
-    image2: 'series_9.png',
-    name: 'Vikings',
-    rating: 5,
-  },
-  {
-    image1: 'series_9.png',
-    image2: 'series_1.png',
-    name: 'Dexter',
-    rating: 5,
-  },
-  {
-    image1: 'series_10.png',
-    image2: 'series_1.png',
-    name: 'Dexter',
-    rating: 5,
-  },
-];
+const loadSeries = async () => {
+  const seriesContainer = document.querySelector('.movieList');
 
-series.forEach(serie => {
-  seriesContainer.innerHTML += `
+  const series = await fetchSeries();
+
+  series.forEach(serie => {
+    seriesContainer.innerHTML += `
   <my-movie-card
   onmouseover="movieHoverEffect(event)"
   onmouseleave="movieMouseLeaveEffect(event)"
@@ -75,4 +26,7 @@ series.forEach(serie => {
   name="${serie.name}"
   ></my-movie-card>
 `;
-});
+  });
+};
+
+loadSeries();
