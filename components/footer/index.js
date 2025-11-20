ee281088e5cf46827baea99df502d5fc89cd0c52
Suela Zeneli <suelazeneli2@gmail.com>
2025-11-20 21:16:38 +0100

    added footer component

---
+++ components/footer/index.js
@@ -0,0 +1,43 @@
+class Footer extends HTMLElement {
+  
+  async connectedCallback() {
+    await this.render();
+  }
+
+  
+  async render() {
+    this.innerHTML = `<link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="global.css">
+     <footer>
+  <div class="footerLinks">
+    <div class="firstRow">
+      <a href="#">Get the Omni App <my-icon iconName="chevronRight"></my-icon></a>
+      <a href="#">Help <my-icon iconName="chevronRight"></my-icon></a>
+      <a href="#">Site Index <my-icon iconName="chevronRight"></my-icon></a>
+      <a href="#">Omni Pro <my-icon iconName="chevronRight"></my-icon></a>
+      <a href="#">Advertising <my-icon iconName="chevronRight"></my-icon></a>
+    </div>
+
+    <div class="secondRow">
+      <a href="#">Omni Developer <my-icon iconName="chevronRight"></my-icon></a>
+      <a href="#">Jobs <my-icon iconName="chevronRight"></my-icon></a>
+      <a href="#">Privacy Policy <my-icon iconName="chevronRight"></my-icon></a>
+    </div>
+  </div>
+
+  <div class="contactIcons">
+    <a href="#" class="contactIcon"><my-icon iconName="facebook"></my-icon></a>
+    <a href="#" class="contactIcon"><my-icon iconName="instagram"></my-icon></a>
+    <a href="#" class="contactIcon"><my-icon iconName="linkedin"></my-icon></a>
+    <a href="#" class="contactIcon"><my-icon iconName="youtube"></my-icon></a>
+    <a href="#" class="contactIcon"><my-icon iconName="telegram"></my-icon></a>
+  </div>
+</footer>
+
+        
+    `;
+  }
+}
+
+
+customElements.define('my-footer', footerComponent);
